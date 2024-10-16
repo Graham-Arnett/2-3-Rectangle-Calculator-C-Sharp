d8a72475077bd08dfb0ca866833c9ae1ce83f990
Graham Arnett <GrahamKentArnett@outlook.com>
2024-10-16 15:28:06 -0400

    init commit

---
+++ Rectangle Calculator/Rectangle Calculator/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Rectangle_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the rectangle calculator");
+            String choice;
+            do
+            {
+                Console.Write("Enter width: ");
+                double width = double.Parse(Console.ReadLine());
+
+                Console.Write("Enter length: ");
+                double length = double.Parse(Console.ReadLine());
+
+                double area = length * width;
+
+                double perimeter = (length * 2) + (width * 2);
+
+                Console.WriteLine($"Area: {area}");
+                Console.WriteLine($"Perimeter: {perimeter}");
+
+                Console.Write("Would you like to go again? (y/n): ");
+                choice = Console.ReadLine().ToLower().Trim();
+            }while(choice == "y");
+        }
+    }
+}
